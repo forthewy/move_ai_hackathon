6b7b8d8a1defd9855f376faf34a69f9013b2e986
DESKTOP-J8RJJS7\jane <forthewy@gmail.com>
2026-08-13 11:23:56 +0900

    no message

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import { execFile } from "child_process";
 import { createServer as createViteServer } from "vite";
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
